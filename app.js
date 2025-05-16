04e9faccb3f4546eb0fa3388dcf5e4204bad0329
Tithy <anzumtithy61@gmail.com>
2025-05-15 21:47:51 -0500

    Cookie Parser

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const url = 'mongodb://127.0.0.1:27017/nucampsite';
 const connect = mongoose.connect(url);
 
 connect.then(() => console.log('Connected correctly to MongoDB server'))
-      .catch(err => console.log('MongoDB connection error:', err));
+  .catch(err => console.log('MongoDB connection error:', err));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -27,60 +27,67 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 //custom middleware function auth
 
-function auth(req,res,next) {
+function auth(req, res, next) {
+    if (!req.signedCookies.user) {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            const err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
 
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    const err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
-
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  const user = auth[0];
-  const pass = auth[1];
-
-  if (user === 'admin' && pass === 'password') {
-    return next(); //authorized
-  } else {
-    const err = new Error('You are not authnticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err); //unauthorized
-  }
-
+        const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+        const user = auth[0];
+        const pass = auth[1];
+        if (user === 'admin' && pass === 'password') {
+            res.cookie('user', 'admin', {signed: true});
+            return next(); // authorized
+        } else {
+            const err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+    } else {
+        if (req.signedCookies.user === 'admin') {
+            return next();
+        } else {
+            const err = new Error('You are not authenticated!');
+            err.status = 401;
+            return next(err);
+        }
+    }
 }
 
-app.use(auth);
+  app.use(auth);
 
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
+  app.use('/', indexRouter);
+  app.use('/users', usersRouter);
+  app.use('/campsites', campsiteRouter);
+  app.use('/promotions', promotionRouter);
+  app.use('/partners', partnerRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
-module.exports = app;
+  module.exports = app;
